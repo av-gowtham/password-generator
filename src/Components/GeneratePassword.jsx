a1fcce3b4b106861088e497dbdd57221964341a8
av-gowtham <gowthamavg10@gmail.com>
2024-07-17 13:07:52 +0530

    Added the checking functionality of the handleGenerate function

--- src/Components/GeneratePassword.jsx
+++ src/Components/GeneratePassword.jsx
@@ -9,23 +9,78 @@ function GeneratePassword() {
   const [password, setPassword] = useState("");
 
   function handleGenerate() {
-    let charSet = "";
-    if (upperCheckbox) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (lowerCheckbox) charSet += "abcdefghijklmnopqrstuvwxyz";
-    if (numberCheckbox) charSet += "01234567890123456789";
-    if (symbolCheckbox) charSet += "!@#$%^&*()_-+={}[]|/:;.,<>?~`";
+    if (length < 4) {
+      const selectedOptionsCount =
+        (upperCheckbox ? 1 : 0) +
+        (lowerCheckbox ? 1 : 0) +
+        (numberCheckbox ? 1 : 0) +
+        (symbolCheckbox ? 1 : 0);
 
-    if (!upperCheckbox && !lowerCheckbox && !numberCheckbox && !symbolCheckbox)
-      return alert("Check any one of the boxes");
-
-    let generatedPassword = "";
-
-    for (let i = 0; i < length; i++) {
-      const index = Math.floor(Math.random() * charSet.length);
-      generatedPassword += charSet[index];
+      if (selectedOptionsCount > length) {
+        alert("Checkbox and Password length are mismatching");
+        return;
+      }
     }
 
+    if (
+      !upperCheckbox &&
+      !lowerCheckbox &&
+      !numberCheckbox &&
+      !symbolCheckbox
+    ) {
+      alert("Check any one of the boxes");
+      return;
+    }
+
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_-+={}[]|/:;.,<>?~`";
+
+    let charSet = "";
+    if (upperCheckbox) charSet += uppercase;
+    if (lowerCheckbox) charSet += lowercase;
+    if (numberCheckbox) charSet += numbers;
+    if (symbolCheckbox) charSet += symbols;
+
+    let generatedPassword = generatePassword();
+
+    while (!passwordMeetsRequirement(generatedPassword)) {
+      generatedPassword = generatePassword();
+    }
+
+    console.log("Generated Password:", generatedPassword);
     setPassword(generatedPassword);
+
+    function generatePassword() {
+      let password = "";
+      for (let i = 0; i < length; i++) {
+        const index = Math.floor(Math.random() * charSet.length);
+        password += charSet[index];
+      }
+      return password;
+    }
+
+    function passwordMeetsRequirement(password) {
+      let hasUppercase = false;
+      let hasLowercase = false;
+      let hasNumber = false;
+      let hasSymbol = false;
+
+      for (let char of password) {
+        if (uppercase.includes(char)) hasUppercase = true;
+        if (lowercase.includes(char)) hasLowercase = true;
+        if (numbers.includes(char)) hasNumber = true;
+        if (symbols.includes(char)) hasSymbol = true;
+      }
+
+      return (
+        (!upperCheckbox || hasUppercase) &&
+        (!lowerCheckbox || hasLowercase) &&
+        (!numberCheckbox || hasNumber) &&
+        (!symbolCheckbox || hasSymbol)
+      );
+    }
   }
 
   function handleCopy() {
